20140578064d9631b19a741e75e2114788ad51fd
sukidasakura <mashencai@126.com>
2017-12-04 15:40:15 +0800

    替换Kafka安装包及修改相应脚本（添加和配置kafka-manager）、修改集群安装文档

--- service/logconfig.sh
+++ service/logconfig.sh
@@ -8,15 +8,10 @@
 ## Created:       2017-11-23
 ################################################################################
 
-#set -x
-
-#---------------------------------------------------------------------#
-#                              定义变量                                #
-#---------------------------------------------------------------------#
-
+set -x
 
 cd `dirname $0` ## 进入当前目录
-BIN_DIR=`pwd` ## 脚本所在目录：install
+BIN_DIR=`pwd` ## 脚本所在目录：service
 cd ..
 ROOT_HOME=`pwd` ## 安装包根目录
 CONF_DIR=${ROOT_HOME}/conf ## 配置文件目录：conf
@@ -33,6 +28,7 @@ ZK_LOG_PATH=${LOGS_PATH}/zookeeper
 HADOOP_LOG_PATH=${LOGS_PATH}/hadoop
 
 
+
 #创建总日志目录及es日志目录，并设置es目录权限，这样执行es启动脚本才能启动成功：
 for hostname in $(cat ${CONF_DIR}/hostnamelists.properties);do
 	ssh root@${hostname} "mkdir -p ${LOGS_PATH};mkdir -p ${ES_LOG_PATH};chmod -R 777 ${ES_LOG_PATH}"
@@ -43,6 +39,4 @@ for hostname in $(cat ${CONF_DIR}/hostnamelists.properties);do
 	ssh root@${hostname} "mkdir -p ${ES_LOG_PATH};mkdir -p ${ROCKETMQ_LOG_PATH};mkdir -p ${SPARK_LOG_PATH};mkdir -p ${KAFKA_LOG_PATH};mkdir -p ${HIVE_LOG_PATH};mkdir -p ${HBASE_LOG_PATH};mkdir -p ${ZK_LOG_PATH};mkdir -p ${HADOOP_LOG_PATH}"
 done
 
-
-
-set +x	
+set +x
